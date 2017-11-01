2ebc0272105b203fc105072b417ef31f36a03989
mechero <moises.macero@gmail.com>
2017-11-01 08:16:58 +0100

    Fixing stale files committed by mistake.

--- angular-reactive/src/app/app.component.ts
+++ angular-reactive/src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { selector } from 'rxjs/operator/multicast';
+import { SelectControlValueAccessor } from '@angular/forms/src/directives';
+import { QuotesComponent } from './quotes.component';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-quotes',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Reactive Quotes';
 }
--- angular-reactive/src/app/app.module.ts
+++ angular-reactive/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { QuoteReactiveService } from './quote-reactive.service';
+import { QuoteService } from './quote.service';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes.component';
+import { QuoteListComponent } from './quote-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    QuoteService,
+    QuoteReactiveService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
