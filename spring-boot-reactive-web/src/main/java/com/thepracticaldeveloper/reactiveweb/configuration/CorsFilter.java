7401ad2ca1e369f608d17aa793006102e2754435
mechero <moises.macero@gmail.com>
2017-11-29 08:09:40 +0100

    Adding basic Docker configuration.

--- spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/configuration/CorsFilter.java
+++ spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/configuration/CorsFilter.java
@@ -15,14 +15,19 @@ public class CorsFilter implements WebFilter {
     public Mono<Void> filter(final ServerWebExchange serverWebExchange, final WebFilterChain webFilterChain) {
         // Adapted from https://sandstorm.de/de/blog/post/cors-headers-for-spring-boot-kotlin-webflux-reactor-project.html
         serverWebExchange.getResponse().getHeaders().add("Access-Control-Allow-Origin", "*");
-        serverWebExchange.getResponse().getHeaders().add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-        serverWebExchange.getResponse().getHeaders().add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range");
+        serverWebExchange.getResponse().getHeaders().add("Access-Control-Allow-Methods",
+                "GET, PUT, POST, DELETE, OPTIONS");
+        serverWebExchange.getResponse().getHeaders().add("Access-Control-Allow-Headers",
+                "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With," +
+                        "If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range");
         if (serverWebExchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
             serverWebExchange.getResponse().getHeaders().add("Access-Control-Max-Age", "1728000");
             serverWebExchange.getResponse().setStatusCode(HttpStatus.NO_CONTENT);
             return Mono.empty();
         } else {
-            serverWebExchange.getResponse().getHeaders().add("Access-Control-Expose-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range");
+            serverWebExchange.getResponse().getHeaders().add("Access-Control-Expose-Headers",
+                    "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With," +
+                            "If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range");
             return webFilterChain.filter(serverWebExchange);
         }
     }
--- spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/repository/QuoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thepracticaldeveloper.reactiveweb.repository;
-
-import com.thepracticaldeveloper.reactiveweb.domain.Quote;
-import reactor.core.publisher.Flux;
-
-public interface QuoteRepository {
-
-    Flux<Quote> retrieveAllQuotes();
-}
