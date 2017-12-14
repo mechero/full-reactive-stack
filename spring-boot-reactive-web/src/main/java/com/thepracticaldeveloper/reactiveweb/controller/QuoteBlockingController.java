ae113afd1d4ec92cd6f8defc59b20ca4270c0161
Moises Macero <moises.macero@gmail.com>
2017-12-14 10:15:29 +0100

    Splitting the controller

---
+++ spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/controller/QuoteBlockingController.java
@@ -0,0 +1,33 @@
+package com.thepracticaldeveloper.reactiveweb.controller;
+
+import com.thepracticaldeveloper.reactiveweb.domain.Quote;
+import com.thepracticaldeveloper.reactiveweb.repository.QuoteMongoBlockingRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuoteBlockingController {
+
+    private static final int DELAY_PER_ITEM_MS = 100;
+
+    private QuoteMongoBlockingRepository quoteMongoBlockingRepository;
+
+    public QuoteBlockingController(final QuoteMongoBlockingRepository quoteMongoBlockingRepository) {
+        this.quoteMongoBlockingRepository = quoteMongoBlockingRepository;
+    }
+
+    @GetMapping("/quotes-blocking")
+    public Iterable<Quote> getQuotesBlocking() throws Exception {
+        Thread.sleep(DELAY_PER_ITEM_MS * quoteMongoBlockingRepository.count());
+        return quoteMongoBlockingRepository.findAll();
+    }
+
+    @GetMapping("/quotes-blocking-paged")
+    public Iterable<Quote> getQuotesBlocking(final @RequestParam(name = "page") int page,
+                                             final @RequestParam(name = "size") int size) throws Exception {
+        Thread.sleep(DELAY_PER_ITEM_MS * size);
+        return quoteMongoBlockingRepository.retrieveAllQuotesPaged(PageRequest.of(page, size));
+    }
+}
--- spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/controller/QuoteReactiveController.java
+++ spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/controller/QuoteReactiveController.java
@@ -1,10 +1,7 @@
 package com.thepracticaldeveloper.reactiveweb.controller;
 
 import com.thepracticaldeveloper.reactiveweb.domain.Quote;
-import com.thepracticaldeveloper.reactiveweb.repository.QuoteMongoBlockingRepository;
 import com.thepracticaldeveloper.reactiveweb.repository.QuoteMongoReactiveRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,19 +11,14 @@ import reactor.core.publisher.Flux;
 import java.time.Duration;
 
 @RestController
-public class QuoteController {
+public class QuoteReactiveController {
 
     private static final int DELAY_PER_ITEM_MS = 100;
 
-    private static final Logger log = LoggerFactory.getLogger(QuoteController.class);
-
     private QuoteMongoReactiveRepository quoteMongoReactiveRepository;
-    private QuoteMongoBlockingRepository quoteMongoBlockingRepository;
 
-    public QuoteController(final QuoteMongoReactiveRepository quoteMongoReactiveRepository,
-                           final QuoteMongoBlockingRepository quoteMongoBlockingRepository) {
+    public QuoteReactiveController(final QuoteMongoReactiveRepository quoteMongoReactiveRepository) {
         this.quoteMongoReactiveRepository = quoteMongoReactiveRepository;
-        this.quoteMongoBlockingRepository = quoteMongoBlockingRepository;
     }
 
     @GetMapping("/quotes-reactive")
@@ -42,16 +34,4 @@ public class QuoteController {
                 .delayElements(Duration.ofMillis(DELAY_PER_ITEM_MS));
     }
 
-    @GetMapping("/quotes-blocking")
-    public Iterable<Quote> getQuotesBlocking() throws Exception {
-        Thread.sleep(DELAY_PER_ITEM_MS * quoteMongoBlockingRepository.count());
-        return quoteMongoBlockingRepository.findAll();
-    }
-
-    @GetMapping("/quotes-blocking-paged")
-    public Iterable<Quote> getQuotesBlocking(final @RequestParam(name = "page") int page,
-                                             final @RequestParam(name = "size") int size) throws Exception{
-        Thread.sleep(DELAY_PER_ITEM_MS * size);
-        return quoteMongoBlockingRepository.retrieveAllQuotesPaged(PageRequest.of(page, size));
-    }
 }
